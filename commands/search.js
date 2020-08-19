e6b9964940bc34d9166dcee04eb3f48125d44926
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-19 11:47:58 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -11,37 +11,39 @@ request('https://api.jikan.moe/v3/search/{type}?q=Fate/Zero&page=1', function (e
 module.exports.run = async (bot, message, args) => {
     let type = args[0];
     let maxAmount = args.length;
-    let whatSearchingFor;
+    var whatSearchingFor;
     console.log(args[0]);
     console.log(args[1]);
     console.log(args[2]);
     console.log(maxAmount);
-        whatSearchingFor = args[1];
-        let i = 2;
-        while(i < maxAmount){
-            whatSearchingFor = whatSearchingFor+" "+args[i];
-            console.log(whatSearchingFor);
-            i++;
-        }
+    whatSearchingFor = args[1];
+    let i = 2;
+    while(i < maxAmount){
+        whatSearchingFor = whatSearchingFor+" "+args[i];
         console.log(whatSearchingFor);
-    if(((args[0]) !=="tv")||((args[0]) !=="ova")||((args[0]) !=="movie")||((args[0]) !=="manga")||((args[0]) !=="novel")){
-        return message.reply("Enter a correct type after the command to search. The correct types are tv, ova, movie, manga and novel").then(message  => { message.delete({ timeout: 10000 }) }).catch(console.error);
-    }else{
-
-        try{
-            request('https://api.jikan.moe/v3/search/{'+type+'}?q='+whatSearchingFor+'&page=1', function (error, response, body) {
-                console.log('Status:', response.statusCode);
-                console.log('Headers:', JSON.stringify(response.headers));
-                console.log('Response:', body);
-            });
-        }
-        catch(err){
-            console.log(err);
-        }
+        i++;
     }
-    
-   
-    return message.reply("<:secre_yes:743119658181197834>");
+    console.log(whatSearchingFor);
+    whatSearchingFor = whatSearchingFor.toLowerCase();
+    console.log(whatSearchingFor);
+    if((type) ==="tv"){
+        return getAnimeMangaSoOn(type,whatSearchingFor);
+    }
+    else if((type) ==="ova"){
+        return getAnimeMangaSoOn(type,whatSearchingFor);
+    }
+    else if((type) ==="movie"){
+        return getAnimeMangaSoOn(type,whatSearchingFor);
+    }
+    else if((type) ==="manga"){
+        return getAnimeMangaSoOn(type,whatSearchingFor);
+    }
+    else if((type) ==="novel"){
+        return getAnimeMangaSoOn(type,whatSearchingFor);
+    }
+    else{
+        return message.reply("Enter a correct type after the command to search. The correct types are tv, ova, movie, manga and novel").then(message  => { message.delete({ timeout: 10000 }) }).catch(console.error);
+    }
 }
 
 module.exports.config = {
@@ -50,4 +52,17 @@ module.exports.config = {
     usage: ".search",
     accessableby: "Members",
     aliases: ['search']
+}
+
+function getAnimeMangaSoOn(type,search){
+    try{
+        request('https://api.jikan.moe/v3/search/{'+type+'}?q='+search+'&page=1', function (error, response, body) {
+            console.log('Status:', response.statusCode);
+            console.log('Headers:', JSON.stringify(response.headers));
+            console.log('Response:', body);
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
 }
