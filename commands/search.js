5b9d6cbd98c64e1c6b735e1e1d92a925152936f9
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-19 11:19:13 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -11,15 +11,21 @@ request('https://api.jikan.moe/v3/search/{type}?q=Fate/Zero&page=1', function (e
 module.exports.run = async (bot, message, args) => {
     let type = args[0];
     let maxAmount = args.length;
-    let whatSearchingFor = args[1];
+    let whatSearchingFor;
     console.log(args[0]);
     console.log(args[1]);
     console.log(args[2]);
     console.log(args[3]);
     console.log(maxAmount);
+        let i = 1;
+        while(i <= maxAmount){
+            whatSearchingFor = whatSearchingFor+" "+args[i];
+        }
+        console.log(whatSearchingFor);
     if(((args[0]) !="tv")||((args[0]) !="ova")||((args[0]) !="movie")||((args[0]) !="manga")||((args[0]) !="novel")){
         return message.reply("Enter a correct type after the command to search. The correct types are tv, ova, movie, manga and novel").then(message  => { message.delete({ timeout: 10000 }) }).catch(console.error);
     }else{
+
         try{
             request('https://api.jikan.moe/v3/search/{'+type+'}?q='+whatSearchingFor+'&page=1', function (error, response, body) {
                 console.log('Status:', response.statusCode);
