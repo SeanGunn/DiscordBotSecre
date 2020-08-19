fa7c9c7e676f84ab12537686fa91b08ae69a8a15
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-19 13:03:33 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -56,10 +56,11 @@ function getAnimeMangaSoOn(message,type,search,trueName){
     try{
         request('https://api.jikan.moe/v3/search/'+type+'?q='+search+'&page=1', function (error, response, body) {
             var t = JSON.parse(body);
-            var sizeTues = Object.keys(t.results).length;
-            if(sizeTues == null){
+           
+            if(Object.keys(t.results).length == null){
                 return message.reply("Make sure the spelling is correct for the search to work or the search value just doesn't exist.");
             }else{
+                var sizeTues = Object.keys(t.results).length;
                     if(sizeTues >= 3){
                     var string = "__**The top 3 "+type+" for the search "+trueName+" are: **__\n";
                     message.channel.send(string);
