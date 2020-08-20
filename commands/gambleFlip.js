be62765e62068e9d71ff286f43f9a0bae7293d96
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-20 10:32:28 +0100

    Update gambleFlip.js

--- commands/gambleFlip.js
+++ commands/gambleFlip.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
     }
     console.log(args[0]);
     console.log(args[1]);
-    if((args[1] != "t" || "h")){
+    if((args[1] != ("t" || "h"))){
         return message.reply("Enter a t or h after the command and amount to gamble.").then(message  => { message.delete({ timeout: 10000 }) }).catch(console.error);
 
     }else if (parseInt(args[0]) <= 0) {
