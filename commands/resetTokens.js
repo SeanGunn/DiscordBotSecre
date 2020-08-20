38a27c019c5bc0c29ea698bec320e8be64b4720b
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-20 13:37:17 +0100

    Can now reset tokens back to base

--- commands/resetTokens.js
+++ commands/resetTokens.js
@@ -5,68 +5,28 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://anyUser:A8aCI8lJ14aHILT3@cluster0.wfkj0.mongodb.net/SecreBot?retryWrites=true&w=majority";
 
 module.exports.run = async (bot, message, args) => {
-    var string;
     var client = new MongoClient(uri, { useNewUrlParser: true });
     try{
-        console.log(message.member.id);
         await client.connect();
-        checkUserNew(client,message.member.id);
-        console.log("here 1");
-        string = await tokensOwn(client,message.member.id);
+        var updatedInformation ={
+            user: userId,
+            tokens: 1000
+        };
+        var result = await client.db("SecreBot").collection("Tokens").updateOne({user: userId},{$set: updatedInformation});
+        console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+        console.log(`${result.modifiedCount} document(s) was/were updated.`);
     }catch(err){
         console.error(err);
     }finally{
         await client.close();
     }
-    return message.reply(string);
+    return message.reply("**Your tokens was reset back to 1000.**");
 }
 
 module.exports.config = {
-    name: "mReset",
-    description: "Resets tokens to 1000",
-    usage: ".mReset",
+    name: "resetTokens",
+    description: "Gamble tokens based on a call and win or lose tokens based on what comes back",
+    usage: ".resetTokens",
     accessableby: "Members",
-    aliases: ['mReset']
-}
-
-async function createUser(client, user){
-    const result = await client.db("SecreBot").collection("Tokens").insertOne(user);
-    console.log(`New user created with the following id: ${result.insertedId}`);
-}
-
-async function checkUserNew(client,userId){
-console.log("user id is: "+ userId);
-   var result = await client.db("SecreBot").collection("Tokens").findOne({user: userId});
-   if(result){
-       console.log("Found a user already 1");
-       console.log(result);
-   }else{
-       console.log("New user");
-       await createUser(client,{
-        user: userId,
-        tokens: 1000
-        });
-   }
-}
-
-async function tokensOwn(client,userId){
-    var tokensAmount;
-    console.log("here 3");
-    try {
-        console.log("here 4");
-        var result = await client.db("SecreBot").collection("Tokens").findOne({user: userId});
-        console.log("here 5");
-        if(result){
-            console.log("Found a user already 2");
-            console.log(result);
-            tokensAmount = result.tokens;
-            console.log(tokensAmount);
-            return tokensAmount;
-        }else{
-            console.log("Their was a error finding the user.");
-        }
-    }catch(err){
-        console.error(err);
-    }
-    return "Their was a error finding your token amount.";
+    aliases: ['resetTokens']
 }
