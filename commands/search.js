e70b64e9255e1ce5581999f660d182c6dd31cdf8
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-20 18:12:34 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -8,7 +8,9 @@ module.exports.run = async (bot, message, args) => {
     var whatSearchingFor;
     var trueName;
     whatSearchingFor = args[1];
-    let i = 1;
+    let i = 3;
+    if(maxAmount > 3)
+        whatSearchingFor = args[2];
     while(i < maxAmount){
         whatSearchingFor = whatSearchingFor+"%20"+args[i];
         trueName  = trueName+" "+args[i];
