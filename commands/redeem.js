b5b3238dbaee3617123c6c55dcc839654628b248
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-28 18:33:56 +0100

    Update redeem.js

--- commands/redeem.js
+++ commands/redeem.js
@@ -4,7 +4,7 @@ const uri = "mongodb+srv://anyUser:A8aCI8lJ14aHILT3@cluster0.wfkj0.mongodb.net/S
 module.exports.run = async (bot, message, args) => {
     console.log("redeem 1?");
     let string;
-    const result;
+    let result;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     try{
         console.log("redeem?");
