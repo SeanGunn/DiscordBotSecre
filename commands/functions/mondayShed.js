a8119d58ecc08705c4491d6e461035079d4ae7dd
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-28 11:35:01 +0100

    Update mondayShed.js

--- commands/functions/mondayShed.js
+++ commands/functions/mondayShed.js
@@ -4,6 +4,7 @@ var request = require('request');
     let mondayShed = "";
 
     const getMondayShedAsync = (time) => {
+        getRequest();
         return new Promise(resolve => {
             setTimeout(() => resolve(42), time);
         })
@@ -11,7 +12,7 @@ var request = require('request');
 
     //const doSomething = async () => {
     async function getMondayShed() {
-        let answer1 = await getRequest(3000);
+        let answer1 = await getMondayShedAsync(3000);
         console.log("the answer is: "+answer1);
         return answer1;
     }
@@ -55,37 +56,3 @@ async function getRequest() {
             return string;
         })
 }
-/*module.exports = new Promise(function(resolve,reject){
-      async.function(function(response) {
-        request('https://api.jikan.moe/v3/schedule/monday', function (error, response, body) {
-            var t = JSON.parse(body);
-            var sizeTues = Object.keys(t.monday).length;
-            var i = 0;
-            var string = "__**The shows that are on Monday are: **__\n";
-            while(i<sizeTues-1){
-                string = string +("**") +t.monday[i].title +(":**");
-                if(t.monday[i].score != null){
-                    string = string + ("\n      Currently has a score of ")+t.monday[i].score+(" on mal.");
-                }
-                else{
-                    string = string + ("\n      Currently there is not a score on mal.");
-                }
-                i++
-                if(i<sizeTues-1)
-                    string = string + "\n"
-            } 
-            string = string + ("\n**")+t.monday[sizeTues-1].title+(":**");
-            if(t.monday[sizeTues-1].score != null){
-                string = string + ("\n      Currently has a score of ")+t.monday[sizeTues-1].score+(" on mal.");
-            }
-            else{
-                string = string + ("\n      Currently there is not a score on mal.");
-            }
-            string = string +"\n";
-            console.log(string);
-            string = string + "<:secre_pathetic:743119690859020320>";
-            return string;
-        })
-    }
-    
-}*/
