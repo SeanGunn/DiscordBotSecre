e0304d4eb4922fae5bfa06517c5b9eed02b29f4d
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-28 14:35:20 +0100

    Update gambleFlip.js

--- commands/gambleFlip.js
+++ commands/gambleFlip.js
@@ -28,11 +28,13 @@ module.exports.run = async (bot, message, args) => {
         await client.connect();
         await checkUserNew(client,message.member.id);
         string = await tokensOwn(client,message.member.id);
+        console.log("They have "+string);
+        console.log("They gambling "+gambleAmount);
         newValueOfTokens = string - gambleAmount;
         console.log("the new amount of tokens they current have if they lose "+newValueOfTokens);
         await client.close();
         if(newValueOfTokens < 0){
-        return message.reply("Max you can gamble is "+string+".").then(message => { message.delete({ timeout: 10000 }) }).catch(console.error);
+            return message.reply("Max you can gamble is "+string+".").then(message => { message.delete({ timeout: 10000 }) }).catch(console.error);
         }
         return await flipCoins(flipType,message,gambleAmount,message.member.id);
     }catch(err){
