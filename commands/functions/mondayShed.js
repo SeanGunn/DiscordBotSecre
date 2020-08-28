d683e00f6b98f357c0ef15ed8046487bae1a856d
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-08-28 13:16:00 +0100

    Update mondayShed.js

--- commands/functions/mondayShed.js
+++ commands/functions/mondayShed.js
@@ -22,7 +22,36 @@ const mondayF = async function() {
 };*/
 
 const myFun = async() => {
-    let a = await getRequest();
+    var string = "__**The shows that are on Monday are: **__\n";
+    request('https://api.jikan.moe/v3/schedule/monday', function (error, response, body) {
+        var t = JSON.parse(body);
+        var sizeTues = Object.keys(t.monday).length;
+        var i = 0;
+       
+        while(i<sizeTues-1){
+            string = string +("**") +t.monday[i].title +(":**");
+            if(t.monday[i].score != null){
+                string = string + ("\n      Currently has a score of ")+t.monday[i].score+(" on mal.");
+            }
+            else{
+                string = string + ("\n      Currently there is not a score on mal.");
+            }
+            i++
+            if(i<sizeTues-1)
+                string = string + "\n"
+        } 
+        string = string + ("\n**")+t.monday[sizeTues-1].title+(":**");
+        if(t.monday[sizeTues-1].score != null){
+            string = string + ("\n      Currently has a score of ")+t.monday[sizeTues-1].score+(" on mal.");
+        }
+        else{
+            string = string + ("\n      Currently there is not a score on mal.");
+        }
+        string = string +"\n";
+        console.log(string);
+        string = string + "<:secre_pathetic:743119690859020320>";
+        return string;
+    })
     let state = false;
 
     setTimeout(() => {state = true}, 2000);
@@ -30,7 +59,8 @@ const myFun = async() => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if(state) {
-                resolve(a);
+                console.log(string);
+                resolve(string);
             } else {
                 reject('State is false');
             }
